8cbae5314e00adebcac00d2f5184960c9eaef4b7
ayal <ayalgelles@gmail.com>
2017-02-28 15:38:45 +0200

    frontback

--- client/index.js
+++ client/index.js
@@ -53,73 +53,35 @@ export class Front extends React.Component {
     );
 
     return (
-      pageFront
+      <g className="front">
+      <text x={frontX} y={frontY-20} >Page Front</text>
+      {pageFront}
+      </g>
     );
 
   }
-
 }
 
-
-export class App extends React.Component {
+export class Back extends React.Component   {
   constructor(props){
     super(props);
     this.state = {};
-    this._changeColor = _.debounce((e)=> {
-      var colors = JSON.parse(this.props.location.hash.slice(1) || '{}') || {};
-      colors[this.state.focus] = e.target.value;
-      this.context.router.push({pathname: '/', hash: '#' + JSON.stringify(colors)});
-    },100);
-  }
-  
-  changeColor() {
-    return (e)=>{e.persist(); this._changeColor(e);};
   }
 
-  setFocus(focus) {
-    return (() => {
-      this.setState({focus});
-    }).bind(this);
-  }
-  
   render() {
-    const getStyle = function(name) {
-      var ret = {fill: (colors[name]) || 'whiteSmoke'};
-      if (this.state.focus === name) {
-	ret = {...ret, stroke: 'red', strokeWidth:4, strokeDasharray:'none'};
-      }
-      return ret;
-    }.bind(this);
-
-    var colors = JSON.parse(this.props.location.hash.slice(1) || '{}') || {};
-    
     var that = this;
 
-    // front
-    var frontX = 350;
-    var frontY = 200;
-    var frontW = 200;
-    var frontH = 300;
-    var frontProps = {frontX, frontY, frontW, frontH};
-    
-    // page back
-
-    var backX = 50;
-    var backY = 200;
-    var backW = frontW;
-    var backH = frontH;
-
     var getpageback = (x,y,w,h)=>{
-      var fold = 20;
+      var fold = h/16;
       
       var top1q = (
 	<g>
 	  <rect x={x} y={y} width={w} height={fold} />
 	  <polygon points={[[x,y+fold],[x+w/4,y+fold],[x,y+h/4]]} />
-	  <polygon points={[[x+w/4,y+fold],[x+w/4,y+h/4],[x,y+h/4]]} style={getStyle("inUpRightRight")} onClick={that.setFocus('inUpRightRight')} />
-	  <rect x={x+w/4} y={y+fold} width={w/4*2} height={h/4-fold} style={getStyle("outLeftLeft")} onClick={that.setFocus('outLeftLeft')} />
+	  <polygon points={[[x+w/4,y+fold],[x+w/4,y+h/4],[x,y+h/4]]} style={this.props.getStyle("inUpRightRight")} onClick={that.props.setFocus('inUpRightRight')} />
+	  <rect x={x+w/4} y={y+fold} width={w/4*2} height={h/4-fold} style={this.props.getStyle("outLeftLeft")} onClick={that.props.setFocus('outLeftLeft')} />
 	  <polygon points={[[x+w/4*3,y+fold],[x+w,y+fold],[x+w,y+h/4]]}  />
-	  <polygon points={[[x+w/4*3,y+fold],[x+w,y+h/4],[x+w/4*3,y+h/4]]} style={getStyle("inDownRightRight")} onClick={that.setFocus('inDownRightRight')} />
+	  <polygon points={[[x+w/4*3,y+fold],[x+w,y+h/4],[x+w/4*3,y+h/4]]} style={this.props.getStyle("inDownRightRight")} onClick={that.props.setFocus('inDownRightRight')} />
 	</g>
       );
 
@@ -145,15 +107,15 @@ export class App extends React.Component {
 	<g>
 	  <rect x={x} y={y-fold} width={w} height={fold} />
 	  <polygon points={[[x,y-fold],[x+w/4,y-fold],[x,y-h/4]]} />
-	  <polygon points={[[x+w/4,y-fold],[x+w/4,y-h/4],[x,y-h/4]]} style={getStyle("inUpLeftLeft")} onClick={that.setFocus('inUpLeftLeft')} />
-	  <rect x={x+w/4} y={y-h/4} width={w/4*2} height={h/4-fold} style={getStyle("outRightRight")} onClick={that.setFocus('outRightRight')} />
+	  <polygon points={[[x+w/4,y-fold],[x+w/4,y-h/4],[x,y-h/4]]} style={this.props.getStyle("inUpLeftLeft")} onClick={that.props.setFocus('inUpLeftLeft')} />
+	  <rect x={x+w/4} y={y-h/4} width={w/4*2} height={h/4-fold} style={this.props.getStyle("outRightRight")} onClick={that.props.setFocus('outRightRight')} />
 	  <polygon points={[[x+w/4*3,y-fold],[x+w,y-fold],[x+w,y-h/4]]}  />
-	  <polygon points={[[x+w/4*3,y-fold],[x+w,y-h/4],[x+w/4*3,y-h/4]]} style={getStyle("inDownLeftLeft")} onClick={that.setFocus('inDownLeftLeft')} />
+	  <polygon points={[[x+w/4*3,y-fold],[x+w,y-h/4],[x+w/4*3,y-h/4]]} style={this.props.getStyle("inDownLeftLeft")} onClick={that.props.setFocus('inDownLeftLeft')} />
 	</g>
       );
       return {top1q, top2q,bottom2q,bottom1q};
     };
-
+    var {backX, backY, backW, backH} = this.props;
     var {top1q, top2q,bottom2q,bottom1q} =  getpageback(backX, backY, backW, backH);
 
     var pageBack = (
@@ -164,85 +126,194 @@ export class App extends React.Component {
 	{bottom2q}
       </g>
     );
+    
+    return (
+      <g className="back">
+      <text x={backX} y={backY-20} >Page Back</text>
+      {pageBack}
+      </g>
+    );
+
+  }
+}
 
 
-    // wallet in
 
-    var walletInLeftX = 600;
-    var walletInLeftY = 200;
-    var walletInLeftW = frontW / 2;
-    var walletInLeftH = frontW / 2; // ??
+export class WalletIn extends React.Component   {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    var that = this;
 
     var getWalletInLeft = (x,y,w,h) => {
       var walletInLeft = (
 	<g>
-	  <polygon points={[[x,y],[x+w/3*2,y],[x,y+h/3*2]]} style={getStyle("inUpLeftLeft")} onClick={that.setFocus('inUpLeftLeft')} />
-	  <polygon points={[[x+w/3*2,y],[x+w,y],[x+w,y+h/3], [x+w/3,y+h/3]]} style={getStyle("inUpLeftRight")} onClick={that.setFocus('inUpLeftRight')}/>
-	  <polygon points={[[x+w/3,y+h/3], [x+w,y+h/3], [x+w,y+h],[x+w/3*2,y+h], [x+w/3-10,y+h/3+10]]} style={getStyle("inDownLeftRight")} onClick={that.setFocus('inDownLeftRight')}/>
-	  <polygon points={[[x+w/3-10,y+h/3+10], [x+w/3*2,y+h], [x,y+h], [x,y+h/3*2]]} style={getStyle("inDownLeftLeft")} onClick={that.setFocus('inDownLeftLeft')}/>
+	  <polygon points={[[x,y],[x+w/3*2,y],[x,y+h/3*2]]} style={this.props.getStyle("inUpLeftLeft")} onClick={that.props.setFocus('inUpLeftLeft')} />
+	  <polygon points={[[x+w/3*2,y],[x+w,y],[x+w,y+h/3], [x+w/3,y+h/3]]} style={this.props.getStyle("inUpLeftRight")} onClick={that.props.setFocus('inUpLeftRight')}/>
+	  <polygon points={[[x+w/3,y+h/3], [x+w,y+h/3], [x+w,y+h],[x+w/3*2,y+h], [x+w/3-10,y+h/3+10]]} style={this.props.getStyle("inDownLeftRight")} onClick={that.props.setFocus('inDownLeftRight')}/>
+	  <polygon points={[[x+w/3-10,y+h/3+10], [x+w/3*2,y+h], [x,y+h], [x,y+h/3*2]]} style={this.props.getStyle("inDownLeftLeft")} onClick={that.props.setFocus('inDownLeftLeft')}/>
 	</g>
       );
       return walletInLeft;
     };
 
+    var {walletInLeftX, walletInLeftY, walletInLeftW, walletInLeftH} = this.props;
     var walletInLeft = getWalletInLeft(walletInLeftX, walletInLeftY, walletInLeftW, walletInLeftH);
-
-    var walletInRightX = 600 + frontW;
-    var walletInRightY = 200;
-    var walletInRightW = frontW / 2;
-    var walletInRightH = frontW / 2; // ??
-
 
     var getWalletInRight = (x,y,w,h) => {
       var walletInLeft = (
 	<g>
-	  <polygon points={[[x,y],[x-w/3*2,y],[x,y+h/3*2]]} style={getStyle("inUpRightRight")} onClick={that.setFocus('inUpRightRight')} />
-	  <polygon points={[[x-w/3*2,y],[x-w,y],[x-w,y+h/3], [x-w/3,y+h/3]]} style={getStyle("inUpRightLeft")} onClick={that.setFocus('inUpRightLeft')}/>
-	  <polygon points={[[x-w/3,y+h/3], [x-w,y+h/3], [x-w,y+h],[x-w/3*2,y+h], [x-w/3+10,y+h/3+10]]} style={getStyle("inDownRightLeft")} onClick={that.setFocus('inDownRightLeft')}/>
-	  <polygon points={[[x-w/3+10,y+h/3+10], [x-w/3*2,y+h], [x,y+h], [x,y+h/3*2]]} style={getStyle("inDownRightRight")} onClick={that.setFocus('inDownRightRight')}/>
+	  <polygon points={[[x,y],[x-w/3*2,y],[x,y+h/3*2]]} style={this.props.getStyle("inUpRightRight")} onClick={that.props.setFocus('inUpRightRight')} />
+	  <polygon points={[[x-w/3*2,y],[x-w,y],[x-w,y+h/3], [x-w/3,y+h/3]]} style={this.props.getStyle("inUpRightLeft")} onClick={that.props.setFocus('inUpRightLeft')}/>
+	  <polygon points={[[x-w/3,y+h/3], [x-w,y+h/3], [x-w,y+h],[x-w/3*2,y+h], [x-w/3+10,y+h/3+10]]} style={this.props.getStyle("inDownRightLeft")} onClick={that.props.setFocus('inDownRightLeft')}/>
+	  <polygon points={[[x-w/3+10,y+h/3+10], [x-w/3*2,y+h], [x,y+h], [x,y+h/3*2]]} style={this.props.getStyle("inDownRightRight")} onClick={that.props.setFocus('inDownRightRight')}/>
 	</g>
       );
       return walletInLeft;
     };
 
-    
+    var {walletInRightX, walletInRightY, walletInRightW, walletInRightH} = this.props;
     var walletInRight = getWalletInRight(walletInRightX, walletInRightY, walletInRightW, walletInRightH);
 
-    // walletout
-    var walletOutLeftX = 600;
-    var walletOutLeftY = 400;
-    var walletOutLeftW = frontW / 2;
-    var walletOutLeftH = frontW / 2; // ??
+
+    return (
+      <g className="wIn">
+	<text x={walletInLeftX} y={walletInLeftY-20} >Wallet In</text>
+	{walletInRight}
+	{walletInLeft}
+      </g>
+    );
+
+  }
+}
+
+export class WalletOut extends React.Component   {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    var that = this;
 
     var getWalletOut = (x,y,w,h) => {
       var walletOut = (
 	<g>
-	  <rect x={x} y={y} width={w/3*2} height={h} style={getStyle("outLeftLeft")} onClick={that.setFocus('outLeftLeft')} />
-	  <rect x={x+w/3*2} y={y} width={w/3} height={h} style={getStyle("outLeftRight")} onClick={that.setFocus('outLeftRight')} />
-	  <rect x={x+w} y={y} width={w/3} height={h} style={getStyle("outRightLeft")} onClick={that.setFocus('outRightLeft')} />
-	  <rect x={x+w/3*4} y={y} width={w/3*2} height={h} style={getStyle("outRightRight")} onClick={that.setFocus('outRightRight')} />
+	  <rect x={x} y={y} width={w/3*2} height={h} style={this.props.getStyle("outLeftLeft")} onClick={that.props.setFocus('outLeftLeft')} />
+	  <rect x={x+w/3*2} y={y} width={w/3} height={h} style={this.props.getStyle("outLeftRight")} onClick={that.props.setFocus('outLeftRight')} />
+	  <rect x={x+w} y={y} width={w/3} height={h} style={this.props.getStyle("outRightLeft")} onClick={that.props.setFocus('outRightLeft')} />
+	  <rect x={x+w/3*4} y={y} width={w/3*2} height={h} style={this.props.getStyle("outRightRight")} onClick={that.props.setFocus('outRightRight')} />
 	</g>
       );
       return walletOut;
     };
-
+    
+    var {walletOutLeftX, walletOutLeftY, walletOutLeftW, walletOutLeftH} = this.props;
     var walletOut = getWalletOut(walletOutLeftX, walletOutLeftY, walletOutLeftW, walletOutLeftH);
 
+    return (
+      <g className="wIn">
+	<text x={walletOutLeftX} y={walletOutLeftY-20} >Wallet Out</text>
+	{walletOut}
+      </g>
+    );
 
+  }
+}
+
+
+export class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+    this._changeColor = _.debounce((e)=> {
+      console.log('color', e.target.value);
+      var colors = JSON.parse(this.props.location.hash.slice(1) || '{}') || {};
+      colors[this.state.focus] = e.target.value;
+      this.context.router.push({pathname: '/', hash: '#' + JSON.stringify(colors)});
+    },100);
+  }
+  
+  changeColor() {
+    return (e)=>{
+      e.persist();
+      this._changeColor(e);
+    };
+  }
+
+  setFocus(focus) {
+    return (() => {
+      this.setState({focus});
+    }).bind(this);
+  }
+  
+  render() {
+    const getStyle = function(name) {
+      var ret = {fill: (colors[name]) || 'whiteSmoke'};
+      if (this.state.focus === name) {
+	ret = {...ret, stroke: 'red', strokeWidth:4, strokeDasharray:'none'};
+      }
+      return ret;
+    }.bind(this);
+
+    var colors = JSON.parse(this.props.location.hash.slice(1) || '{}') || {};
+    
+    var that = this;
+
+    // front
+    var frontX = 50;
+    var frontY = 50;
+    var frontW = 100;
+    var frontH = frontW*Math.sqrt(2);
+    var frontProps = {frontX, frontY, frontW, frontH};
+    
+    // page back
+
+    var backX = frontX;
+    var backY = 250;
+    var backW = frontW;
+    var backH = frontH;
+    var backProps = {backX, backY, backW, backH};
+
+    // wallet in
+
+    var walletInLeftX = frontX;
+    var walletInLeftY = 450;
+    var walletInLeftW = frontW / 2;
+    var walletInLeftH = frontW / 2; // ??
+    
+    var walletInRightX = walletInLeftX + frontW;
+    var walletInRightY = walletInLeftY;
+    var walletInRightW = frontW / 2;
+    var walletInRightH = frontW / 2; // ??
+
+
+    var winProps = {walletInLeftX, walletInLeftY, walletInLeftW, walletInLeftH, walletInRightX, walletInRightY, walletInRightW, walletInRightH};
+
+    // walletout
+    var walletOutLeftX = frontX;
+    var walletOutLeftY = 550;
+    var walletOutLeftW = frontW / 2;
+    var walletOutLeftH = frontW / 2; // ??
+    var woutProps = {walletOutLeftX, walletOutLeftY, walletOutLeftW, walletOutLeftH};
 
     return (
       <div className="appdiv" >
 	<div>
 	  pick color:
 	  <input type="color" onChange={this.changeColor().bind(this)} />
-	  <div onClick={(x)=>(this.context.router.push({pathname: '/print/front', hash: this.props.location.hash}))}>print front</div>
+	  <div className="button" onClick={(x)=>(this.context.router.push({pathname: '/print/front', hash: this.props.location.hash}))}>print front</div>
+	  <div className="button" onClick={(x)=>(this.context.router.push({pathname: '/print/back', hash: this.props.location.hash}))}>print back</div>
+	  <div className="button" onClick={(x)=>(this.context.router.push({pathname: '/print/frontback', hash: this.props.location.hash}))}>print front+back</div>
 	</div>
 	<svg>
-	  {pageBack}
+	  <Back getStyle={getStyle.bind(this)} setFocus={this.setFocus.bind(this)} {...backProps} />
 	  <Front getStyle={getStyle.bind(this)} setFocus={this.setFocus.bind(this)} {...frontProps} />
-	  {walletInLeft}
-	  {walletInRight}
-	  {walletOut}
+	  <WalletIn getStyle={getStyle.bind(this)} setFocus={this.setFocus.bind(this)} {...winProps} />
+	  <WalletOut getStyle={getStyle.bind(this)} setFocus={this.setFocus.bind(this)} {...woutProps} />
 	</svg>
       </div>
     );
@@ -281,9 +352,54 @@ export class Print extends React.Component {
 
       return (
 	<div className="print appdiv">
-	<svg>
-	  <Front getStyle={getStyle.bind(this)} setFocus={(x)=>((y)=>(y))} {...frontProps} />
-	</svg>
+	  <svg>
+	    <Front getStyle={getStyle.bind(this)} setFocus={(x)=>((y)=>(y))} {...frontProps} />
+	  </svg>
+	</div>
+      );
+    }
+
+    if (this.props.params.which === 'frontback') {
+
+      var frontX = 0;
+      var frontY = 0;
+      var frontW = 794;
+      var frontH = 1123;
+      var frontProps = {frontX, frontY, frontW, frontH};
+
+
+
+      var backX = 0;
+      var backY = 0;
+      var backW = 794;
+      var backH = 1123;
+      var backProps = {backX, backY, backW, backH};
+
+      return (
+	<div className="print appdiv">
+	  <svg>
+	    <Front getStyle={getStyle.bind(this)} setFocus={(x)=>((y)=>(y))} {...frontProps} />
+	    <Back getStyle={getStyle.bind(this)} setFocus={(x)=>((y)=>(y))} {...backProps} />
+	  </svg>
+	</div>
+      );
+    }
+
+
+
+    if (this.props.params.which === 'back') {
+
+      var backX = 0;
+      var backY = 0;
+      var backW = 794;
+      var backH = 1123;
+      var backProps = {backX, backY, backW, backH};
+
+      return (
+	<div className="print appdiv">
+	  <svg>
+	    <Back getStyle={getStyle.bind(this)} setFocus={(x)=>((y)=>(y))} {...backProps} />
+	  </svg>
 	</div>
       );
     }
